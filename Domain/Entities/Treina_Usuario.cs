22bdc88e6d236966f49d0004e1a2f6bee3864af6
David Sousa <david.criciuma@gmail.com>
2021-08-28 00:36:05 -0400

    Entity

--- Domain/Entities/Treina_Usuario.cs
+++ Domain/Entities/Treina_Usuario.cs
@@ -15,31 +15,15 @@ namespace Domain.Entities
         [Required(ErrorMessage = "A senha não pode ser vazia.")]
         public string Senha { get; set; }
 
-        /*[Column("Nome")]
-        public string Nome { get; set; }
-
-        [Column("Usuario_Atualizacao")]
-        public string Usuario_Atualizacao { get; set; }
-
-        [Column("Data_Atualizacao")]
-        public DateTime Data_Atualizacao { get; set; }
-
-        [Column("Senha_Validade")]
-        public DateTime Senha_Validade { get; set; }*/
-
         protected Treina_Usuario()
         {
 
         }
 
-        public Treina_Usuario(string usuario, string senha/*, string nome, string usuario_Atualizacao, DateTime data_Atualizacao, DateTime senha_Validade*/)
+        public Treina_Usuario(string usuario, string senha)
         {
             Usuario = usuario;
             Senha = senha;
-            /*Nome = nome;
-            Usuario_Atualizacao = usuario_Atualizacao;
-            Data_Atualizacao = data_Atualizacao;
-            Senha_Validade = senha_Validade;*/
         }
     }
 }
